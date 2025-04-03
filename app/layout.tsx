0751bf7c8726fdf7b56016e118ed166f74701bf7
Ivan Matcuka <ivanmatcuka@gmail.com>
2025-04-03 23:12:56 +0200

    chore: new logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata() {
     openGraph: {
       title,
       description,
-      images: '/images/logo.png',
+      images: '/images/logo.svg',
     },
   };
 }
--- app/prisoners/page.tsx
+++ app/prisoners/page.tsx
@@ -15,7 +15,7 @@ export async function generateMetadata() {
     openGraph: {
       title,
       description,
-      images: '/images/logo.png',
+      images: '/images/logo.svg',
     },
   };
 }
