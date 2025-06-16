b16f96dbad42d842055db14d58c5af903eeb6fb6
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-06-16 20:50:27 +0200

    feat: add GitHub Actions workflow for automated deployment on merges to development branch

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
-import type { Preview } from '@storybook/react';
 import { ThemeProvider } from '@emotion/react';
+import type { Preview } from '@storybook/react';
 
 import { theme } from '../theming/theme';
 
