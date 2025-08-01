5a950961a7d1ae33a816a34ae9182b5de0047d33
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-08-01 19:43:58 +0200

    fix: flexWrap

--- src/app/prisoner/[slug]/PrisonerArticles.tsx
+++ src/app/prisoner/[slug]/PrisonerArticles.tsx
@@ -8,7 +8,7 @@ type PrisonerArticlesProps = {
 };
 export const PrisonerArticles: FC<PrisonerArticlesProps> = ({ articles }) => (
   <>
-    <Box display="flex" gap={1}>
+    <Box display="flex" flexWrap="wrap" gap={1}>
       {articles?.map((article) => (
         <Box key={article}>
           <Article label={article} />
