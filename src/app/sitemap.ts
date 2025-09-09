4850356bf43cc692eaec9c1f58282cea17623d0d
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-09-09 17:43:14 +0200

    feat: sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,32 @@
+import type { MetadataRoute } from 'next';
+
+const SITE_URL = process.env.NEXT_SITE_URL || 'example.com';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      changeFrequency: 'yearly',
+      lastModified: new Date(),
+      priority: 1,
+      url: `https://${SITE_URL}/donate`,
+    },
+    {
+      changeFrequency: 'yearly',
+      lastModified: new Date(),
+      priority: 1,
+      url: `https://${SITE_URL}/maintainance`,
+    },
+    {
+      changeFrequency: 'weekly',
+      lastModified: new Date(),
+      priority: 0.8,
+      url: `https://${SITE_URL}/prisoners`,
+    },
+    {
+      changeFrequency: 'monthly',
+      lastModified: new Date(),
+      priority: 0.5,
+      url: `https://${SITE_URL}/prisoner`,
+    },
+  ];
+}
